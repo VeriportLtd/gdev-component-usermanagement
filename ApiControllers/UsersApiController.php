0b9bdf0003db41587380186073ddc41276cb2098
Nenad Milosavljevic <nenad.milosavljevic@canicinteractive.com>
2017-11-03 19:00:11 +0100

    Added function GetUsersWithLesserRoles

--- ApiControllers/UsersApiController.php
+++ ApiControllers/UsersApiController.php
@@ -14,69 +14,86 @@ use Gdev\UserManagement\DataManagers\UsersDataManager;
 use Gdev\UserManagement\Models\User;
 use Gdev\UserManagement\Models\UserDetails;
 
-class UsersApiController {
+class UsersApiController
+{
 
-	public static function GetActiveUsers($offset = null, $limit = null) {
-		return UsersDataManager::GetActiveUsers($offset, $limit);
-	}
+    public static function GetActiveUsers($offset = null, $limit = null)
+    {
+        return UsersDataManager::GetActiveUsers($offset, $limit);
+    }
 
-	public static function GetUsers($offset = null, $limit = null) {
-		return UsersDataManager::GetUsers($offset, $limit);
-	}
+    public static function GetUsers($offset = null, $limit = null)
+    {
+        return UsersDataManager::GetUsers($offset, $limit);
+    }
 
-	public static function GetNotApprovedUsers($offset = null, $limit = null) {
-		return UsersDataManager::GetNotApprovedUsers($offset, $limit);
-	}
+    public static function GetNotApprovedUsers($offset = null, $limit = null)
+    {
+        return UsersDataManager::GetNotApprovedUsers($offset, $limit);
+    }
 
-	public static function GetUsersByCurrentStatus($statusId) {
-		return UsersDataManager::GetUsersByCurrentStatus($statusId);
-	}
+    public static function GetUsersByCurrentStatus($statusId)
+    {
+        return UsersDataManager::GetUsersByCurrentStatus($statusId);
+    }
 
-	/**
-	 * @param $userId
-	 * @return User
-	 */
-	public static function GetUserById($userId) {
-		return UsersDataManager::GetUserById($userId);
-	}
+    /**
+     * @param $userId
+     * @return User
+     */
+    public static function GetUserById($userId)
+    {
+        return UsersDataManager::GetUserById($userId);
+    }
 
-	/**
-	 * @param $userName
-	 * @return User
-	 */
-	public static function GetUserByUserName($userName) {
-		return UsersDataManager::GetUserByUserName($userName);
-	}
+    /**
+     * @param $userName
+     * @return User
+     */
+    public static function GetUserByUserName($userName)
+    {
+        return UsersDataManager::GetUserByUserName($userName);
+    }
 
-	public static function InsertUser($model) {
-		return UsersDataManager::InsertUser($model);
-	}
+    public static function InsertUser($model)
+    {
+        return UsersDataManager::InsertUser($model);
+    }
 
 
-	public static function InsertUserDetails($model) {
-		return UserDetailsDataManager::InsertUserDetails($model);
-	}
+    public static function InsertUserDetails($model)
+    {
+        return UserDetailsDataManager::InsertUserDetails($model);
+    }
 
-	public static function UpdateUser($model) {
-		return UsersDataManager::UpdateUser($model);
-	}
+    public static function UpdateUser($model)
+    {
+        return UsersDataManager::UpdateUser($model);
+    }
 
-	public static function DeleteUser($userId) {
-		return UsersDataManager::DeleteUser($userId);
-	}
+    public static function DeleteUser($userId)
+    {
+        return UsersDataManager::DeleteUser($userId);
+    }
 
-	public static function GetUserByEmail($email) {
-		return UsersDataManager::GetUserByEmail($email);
-	}
+    public static function GetUserByEmail($email)
+    {
+        return UsersDataManager::GetUserByEmail($email);
+    }
 
-	public static function Login($email = null, $password = null, $token = null) {
-		if ($token == null) {
-			return Users::Login($email, $password);
-		}
-		else {
-			return Users::LoginWithToken($token);
-		}
-	}
+    public static function Login($email = null, $password = null, $token = null)
+    {
+        if ($token == null) {
+            return Users::Login($email, $password);
+        } else {
+            return Users::LoginWithToken($token);
+        }
+    }
+
+    public static function GetUsersWithLesserRoles($businessId, $roleWeight)
+    {
+        return UsersDataManager::GetUsersWithLesserRoles($businessId, $roleWeight);
+    }
 
 
 }
--- DataManagers/UsersDataManager.php
+++ DataManagers/UsersDataManager.php
@@ -11,53 +11,78 @@ use Gdev\UserManagement\Repositories\UserRolesRepository;
 use Gdev\UserManagement\Repositories\UsersRepository;
 use Gdev\UserManagement\Repositories\UserStatusesRepository;
 
-class UsersDataManager {
+class UsersDataManager
+{
 
-	public static function GetUsers($offset = null, $limit = null) {
-		return UsersRepository::getInstance()->all()->with(["Roles", "Details"])->limit($limit, $offset);
-	}
+    public static function GetUsers($offset = null, $limit = null)
+    {
+        return UsersRepository::getInstance()->all()->with(["Roles", "Details"])->limit($limit, $offset);
+    }
 
-	public static function GetActiveUsers($offset = null, $limit = null) {
-		return UsersRepository::getInstance()->all()->with(["Roles", "Details"])->where(['Active' => 1])->limit($limit, $offset);
-	}
+    public static function GetActiveUsers($offset = null, $limit = null)
+    {
+        return UsersRepository::getInstance()->all()->with(["Roles", "Details"])->where(['Active' => 1])->limit($limit, $offset);
+    }
 
-	public static function GetNotApprovedUsers($offset = null, $limit = null) {
-		return UsersRepository::getInstance()->all()->with(["Roles", "Details"])->where(['Approved' => null])->limit($limit, $offset);
-	}
+    public static function GetNotApprovedUsers($offset = null, $limit = null)
+    {
+        return UsersRepository::getInstance()->all()->with(["Roles", "Details"])->where(['Approved' => null])->limit($limit, $offset);
+    }
 
-	public static function GetUserById($userId) {
-		return UsersRepository::getInstance()->where(['UserId' => $userId])->with(["Roles", "Details"])->first();
-	}
+    public static function GetUserById($userId)
+    {
+        return UsersRepository::getInstance()->where(['UserId' => $userId])->with(["Roles", "Details"])->first();
+    }
 
-	public static function GetUserByUserName($userName) {
-		return UsersRepository::getInstance()->where(['Username' => $userName])->with(["Roles", "Details"])->first();
-	}
+    public static function GetUserByUserName($userName)
+    {
+        return UsersRepository::getInstance()->where(['Username' => $userName])->with(["Roles", "Details"])->first();
+    }
 
-	public static function GetUserByEmail($email) {
-		return UsersRepository::getInstance()->where(['Email' => $email])->with(["Roles", "Details"])->first();
-	}
+    public static function GetUserByEmail($email)
+    {
+        return UsersRepository::getInstance()->where(['Email' => $email])->with(["Roles", "Details"])->first();
+    }
 
-	public static function InsertUser($model) {
-		return UsersRepository::getInstance()->save($model);
-	}
+    public static function InsertUser($model)
+    {
+        return UsersRepository::getInstance()->save($model);
+    }
 
-	public static function UpdateUser($model) {
-		return UsersRepository::getInstance()->save($model);
-	}
+    public static function UpdateUser($model)
+    {
+        return UsersRepository::getInstance()->save($model);
+    }
 
-	public static function DeleteUser($userId) {
-		return UsersRepository::getInstance()->delete(['UserId' => $userId]);
-	}
+    public static function DeleteUser($userId)
+    {
+        return UsersRepository::getInstance()->delete(['UserId' => $userId]);
+    }
 
-	public static function GetUsersByCurrentStatus($statusId) {
-		$result = [];
-		$statuses = UserStatusesRepository::getInstance()->all()->where(["UserStatusTypeId" => $statusId, "DateTo" => null])->with(["User"])->execute();
-		if(count($statuses) > 0) {
-			foreach($statuses as $status) {
-				$result[] = $status->User;
-			}
-		}
-		return $result;
-	}
+    public static function GetUsersByCurrentStatus($statusId)
+    {
+        $result = [];
+        $statuses = UserStatusesRepository::getInstance()->all()->where(["UserStatusTypeId" => $statusId, "DateTo" => null])->with(["User"])->execute();
+        if (count($statuses) > 0) {
+            foreach ($statuses as $status) {
+                $result[] = $status->User;
+            }
+        }
+        return $result;
+    }
+
+    public static function GetUsersWithLesserRoles($businessId, $roleWeight)
+    {
+       // $result = [];
+
+        $users = UsersRepository::getInstance()
+            ->query("
+                SELECT * FROM users u 
+                INNER JOIN user_businesses ub ON ub.UserId = u.UserId AND ub.BusinessId = :businessId 
+                INNER JOIN user_roles ur ON ur.UserId = u.UserId INNER JOIN roles r ON ur.RoleId = r.RoleId AND r.Weight > :roleWeight ", ["businessId" => $businessId, "roleWeight" => $roleWeight])
+            ->execute();
+        return $users;
+
+    }
 
 }
