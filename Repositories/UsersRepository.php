afd358c7fa0a43c86eff48ffc3db25be58f9f16f
Nikola Stojanović <me@stojanovicnikola.com>
2017-11-27 19:41:47 +0100

    Fixed bug with Last logged in user query

--- Repositories/UsersRepository.php
+++ Repositories/UsersRepository.php
@@ -8,13 +8,20 @@ use Data\Repositories\BaseRepository;
  * Class UsersRepository
  * @package Gdev\UserManagement\Repositories
  */
-class UsersRepository extends BaseRepository {
+class UsersRepository extends BaseRepository
+{
 
     const Model = 'Gdev\UserManagement\Models\User';
 
     public static function GetLastLoggedInUser($weight)
     {
         $db = static::getInstance();
+
+        $roles = RolesRepository::getInstance()->all->where(["Weight" >= $weight]);
+        if (empty($roles)) {
+            return null;
+        }
+
         $query = sprintf("SELECT
         user_details.FirstName,
         user_details.LastName,
@@ -23,7 +30,7 @@ class UsersRepository extends BaseRepository {
          FROM user_access_tokens
         JOIN user_roles on user_roles.UserId = user_access_tokens.UserId
         JOIN user_details on user_roles.UserId = user_details.UserId
-        JOIN roles on user_roles.RoleId = roles.RoleId
+        JOIN roles on user_roles.RoleId = Roles.RoleId
         WHERE roles.Weight >= '%s'
         ORDER BY user_access_tokens.StartDate DESC
         LIMIT 1", $weight);
@@ -31,7 +38,7 @@ class UsersRepository extends BaseRepository {
         if (empty($data[0])) {
             return null;
         }
-        return new \LastLoggedUserDTO($data[0]->FirstName,$data[0]->LastName,$data[0]->Image,$data[0]->StartDate);
+        return new \LastLoggedUserDTO($data[0]->FirstName, $data[0]->LastName, $data[0]->Image, $data[0]->StartDate);
 
     }
 }
