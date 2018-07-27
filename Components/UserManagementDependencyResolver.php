af70741f2288088e8890ce646fec7b493243c1d2
nino5 <nino.nikolic@gentlemandev.com>
2018-07-27 13:10:09 +0200

    default and fallback paths - fix

--- Components/UserManagementDependencyResolver.php
+++ Components/UserManagementDependencyResolver.php
@@ -10,8 +10,8 @@ namespace Gdev\UserManagement\Components;
 
 class UserManagementDependencyResolver extends \Backslash\Resolver\DependencyResolver
 {
-    const DEFAULT_PATH = ROOT_PATH . "Data/Models";
+    const DEFAULT_PATH = ROOT_PATH . "Data/Models/";
     const DEFAULT_NAMESPACE = "Data\Models";
-    const FALLBACK_PATH = ROOT_PATH . "/Composer/Bundles/gdev/user-management/Models";
+    const FALLBACK_PATH = ROOT_PATH . "/Composer/Bundles/gdev/user-management/Models/";
     const FALLBACK_NAMESPACE = "Gdev\UserManagement\Models";
 }
