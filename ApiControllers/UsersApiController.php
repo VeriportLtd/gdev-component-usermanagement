355140beeb823bb691a9376dc74f2c17cf11b138
vitko <vidoje.mijalkovic@gentlemandev.com>
2019-09-09 20:19:09 +0200

    Adding Create User and Create User Details method

--- ApiControllers/UsersApiController.php
+++ ApiControllers/UsersApiController.php
@@ -8,12 +8,16 @@
 
 namespace Gdev\UserManagement\ApiControllers;
 
+use Business\Enums\RolesEnum;
+use Business\Security\Crypt;
 use Business\Security\Users;
 use Business\Enums\PermissionsEnum;
 use Gdev\UserManagement\DataManagers\UserDetailsDataManager;
 use Gdev\UserManagement\DataManagers\UsersDataManager;
 use Gdev\UserManagement\Models\User;
 use Gdev\UserManagement\Models\UserDetails;
+use Gdev\UserManagement\Models\UserRole;
+use Gdev\UserManagement\Models\UserStatus;
 
 class UsersApiController
 {
@@ -158,6 +162,68 @@ class UsersApiController
     public static function GetAllUsers()
     {
         return UsersDataManager::GetAllUsers();
+    }
+
+    public static function CreateUser(string $username, string $password, string $email, ?array $userRoles = null, ?int $organizationId = null, ?int $approved = 0, ?int $active = 0, ?int $userStatusId = null, ?string $statusMessage = null): ?User
+    {
+        $user = new User();
+        $user->Email = $email;
+        $user->Password = Crypt::HashPassword($password);
+        $user->UserName = $username;
+        $user->RegistrationDate = new \DateTime();
+        $user->Approved = $approved;
+        $user->Active = $active;
+
+        // checking organization
+        if ($organizationId !== null) {
+            if (!\Security::IsSuperAdmin()) {
+                $currentUser = UsersApiController::GetUserById(Security::GetCurrentUser()->UserId);
+                $organizationId = $currentUser->OrganizationId;
+            }
+        }
+        $user->OrganizationId = $organizationId;
+        $status = true;
+        $userId = UsersApiController::InsertUser($user);
+        $status = $status && $userId > 0;
+
+        if ($status) {
+            $userId = (int)$userId;
+            if ($userRoles !== null) {
+                foreach ($userRoles as $userRoleId) {
+                    $userRoleId = (int)$userRoleId;
+                    if (in_array($userRoleId, RolesEnum::enum(), true)) {
+                        $userRoleModel = new UserRole();
+                        $userRoleModel->UserId = $userId;
+                        $userRoleModel->RoleId = $userRoleId;
+                        $status = UserRolesApiController::InsertUserRole($userRoleModel) && $status;
+                    }
+                }
+            }
+            if ($userStatusId !== null) {
+                $userStatusModel = new UserStatus();
+                $userStatusModel->UserId = $userId;
+                $userStatusModel->UserStatusTypeId = $userStatusId;
+                $userStatusModel->DateFrom = new \DateTime();
+                $userStatusModel->Message = $statusMessage !== null ? $statusMessage : $userStatusModel->Message = 'No Message';
+                $status = UserStatusesApiController::InsertUserStatus($userStatusModel) && $status;
+            }
+        }
+        return $status ? $user : null;
+    }
+
+    public static function CreateUserDetails(int $userId, string $firstName, string $lastName, ?string $dateOfBirth = null, ?int $gender = null, ?string $picture = null): bool
+    {
+        $userDetails = new UserDetails();
+
+        $userDetails->FirstName = $firstName;
+        $userDetails->LastName = $lastName;
+        $userDetails->DateOfBirth = $dateOfBirth ? \DateTime::createFromFormat('m/d/Y', $dateOfBirth) : null;
+        /*    $userDetails->Gender = $gender && in_array($gender, GendersEnum::enum(), true) ? $gender : null;
+            $userDetails->Picture = $picture;*/
+        $userDetails->UserId = $userId;
+
+        return UsersApiController::InsertUserDetails($userDetails) > 0;
+
     }
 
 
